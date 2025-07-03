ddb88fa79a27219749fdcda23dccc4edb5fc68fc
DPSemenov <daniilsemenov779@gmail.com>
2025-07-04 02:02:37 +0300

    add var

--- src/test/java/testik.java
+++ src/test/java/testik.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class testik {
     @Test
     public void Conflict() {
-        System.out.println("This is the best day");
-        Assertions.assertTrue(3 > 2);
+        int i = 3;
+        Assertions.assertTrue(i > 2);
     }
 }
