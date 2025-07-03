e2d1de411cc56a8d50921cced8173465895c48f6
DPSemenov <daniilsemenov779@gmail.com>
2025-07-04 01:58:54 +0300

    add seot

---
+++ src/test/java/testik.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testik {
+    @Test
+    public void Conflict() {
+        System.out.println("This is the best day");
+        Assertions.assertTrue(3 > 2);
+    }
+}
